91c02a8c9b8c2ebd62f646fe7b6bf622522ead4f
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2015-08-16 02:14:23 +0200

    Added comment on missing img-folder, and update to server

--- slides/server.js
+++ slides/server.js
@@ -2,5 +2,4 @@ var connect = require('connect'),
     serveStatic = require('serve-static');
 
 console.log('Presentation starting at port 1704');
-console.log(__dirname);
 connect().use(serveStatic(__dirname)).listen(1704);
