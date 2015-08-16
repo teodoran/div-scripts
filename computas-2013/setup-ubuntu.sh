8b9ce9bc49822d35f97caf6e778d415110a4fbae
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2015-08-16 02:17:54 +0200

    Update on gitignore and removal of old stuff

--- computas-2013/setup-ubuntu.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install g++ curl libssl-dev apache2-utils
-sudo apt-get install git-core
-cd /opt
-sudo git clone git://github.com/ry/node.git
-cd node
-sudo ./configure
-sudo make
-sudo make install
